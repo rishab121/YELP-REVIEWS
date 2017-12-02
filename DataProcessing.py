4e9627cc21a29e33edb3f28c8cd37fa65ff66142
rishab121 <rishabmalik121@gmail.com>
2017-12-02 11:43:33 -0500

    split function

--- DataProcessing.py
+++ DataProcessing.py
@@ -5,7 +5,7 @@ from nltk.corpus import stopwords
 import sklearn as sk
 import string
 
-reviews = pd.read_csv('./dataset/review.csv')
+reviews = pd.read_csv('review.csv')
 #print reviews.shape
 #print "head"
 #print reviews.head()
@@ -23,19 +23,26 @@ reviews_star = w_reviews['stars']
 
 # Given: some String
 # returns: List of the words in the string without punctations and stopwords
-def textProcessing(text):
+def remove_chars(text):
     text_list = [char for char in text if char not in string.punctuation] # will return list of char without puncs.
     text_list = ''.join(text_list) # will convert it into string
+    return remove_stopwords(text_list) # to remove stopwords
+
+
+def remove_stopwords(text_list):
     filtered_list = []  # my list of filtered words
+
     for word in text_list.split():
-        if str.lower(word) not in stopwords.words('english'):
+        if word.lower() not in stopwords.words('english'):
             filtered_list.append(word)
 
     return filtered_list
 
 
-feature_matrix = sk.feature_extraction.text.CountVectorizer(analyzer = textProcessing).fit_transform(reviews_text)
-print feature_matrix.shape
+
+feature_matrix = sk.feature_extraction.text.CountVectorizer(analyzer = remove_chars).fit(reviews_text)
+reviews_text = feature_matrix.transform(reviews_text)
+print reviews_text
 
 
 
