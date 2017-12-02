cce321359eb886917288bf9a44c8ca6cd9518bd0
rishab121 <rishabmalik121@gmail.com>
2017-12-02 12:51:32 -0500

    kk

--- DataProcessing.py
+++ DataProcessing.py
@@ -6,26 +6,30 @@ import sklearn as sk
 import string
 import re
 
-reviews = pd.read_csv('./dataset/review.csv', chunksize = 10000)
+reviews = pd.read_csv('./dataset/review.csv',chunksize = 10000)
+min_reviews = reviews[0:10000]
 
-def textProcessing(text):
-	letters_only = re.sub('[^a-zA-Z]', ' ', text)
+
+def remove_punc(text):
+	letters_only = re.sub('[^a-zA-Z]', ' ',text)
 	words = letters_only.lower().split()
+	return remove_stopwords(words)
+
+def remove_stopwords(words):
 	stopwords_eng = set(stopwords.words("english"))
 	useful_words = [word for word in words if not word in stopwords_eng]
 	return(useful_words)
 
-for min_reviews in reviews:
-	w_reviews = min_reviews[(min_reviews['stars'] == 1) | (min_reviews['stars'] == 5)]
 
-	reviews_text = w_reviews['text']
-	reviews_star = w_reviews['stars']
+w_reviews = min_reviews[(min_reviews['stars'] == 1) | (min_reviews['stars'] == 5)]
+reviews_text = w_reviews['text']
+reviews_star = w_reviews['stars']
 	
-	feature_matrix = sk.feature_extraction.text.CountVectorizer(analyzer = textProcessing).fit(reviews_text)
-	reviews_text = feature_matrix.transform(reviews_text)
+feature_matrix = sk.feature_extraction.text.CountVectorizer(analyzer = remove_punc).fit(reviews_text)
+reviews_text = feature_matrix.transform(reviews_text)
+print reviews_text.shape
 
 
-	break
 
 
 
