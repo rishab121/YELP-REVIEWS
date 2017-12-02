6efe05e1b5cd9664de0fee3cb0ffa51ad4415a97
Abhinav Kharbanda <abhinav.kharbanda25@gmail.com>
2017-12-02 12:23:13 -0500

    Faster text processing

--- DataProcessing.py
+++ DataProcessing.py
@@ -4,39 +4,26 @@ import nltk
 from nltk.corpus import stopwords
 import sklearn as sk
 import string
+import re
 
-reviews = pd.read_csv('./dataset/review.csv')
-#print reviews.shape
-#print "head"
-#print reviews.head()
-#print "info"
-#info = reviews.info()
-#print  reviews.describe()
-# very large dataset will consider for upto 10000 reviews
-min_reviews = reviews[0:10000]
-w_reviews = min_reviews[(min_reviews['stars'] == 1) | (min_reviews['stars'] == 5)]
+reviews = pd.read_csv('./dataset/review.csv', chunksize = 10000)
 
-#pd.DataFrame.to_csv(w_reviews)
-reviews_text = w_reviews['text']
-reviews_star = w_reviews['stars']
-# have to remove stopwords and punctation in text
 
-# Given: some String
-# returns: List of the words in the string without punctations and stopwords
 def textProcessing(text):
-    text_list = [char for char in text if char not in string.punctuation] # will return list of char without puncs.
-    text_list = ''.join(text_list) # will convert it into string
-    filtered_list = []  # my list of filtered words
-    for word in text_list.split():
-        if str.lower(word) not in stopwords.words('english'):
-            filtered_list.append(word)
+	letters_only = re.sub('[^a-zA-Z]', ' ', text)
+	words = letters_only.lower().split()
+	stopwords_eng = set(stopwords.words("english"))
+	useful_words = [word for word in words if not word in stopwords_eng]
+	return(useful_words)
 
-    return filtered_list
+for min_reviews in reviews:
+	w_reviews = min_reviews[(min_reviews['stars'] == 1) | (min_reviews['stars'] == 5)]
+
+	reviews_text = w_reviews['text']
+	reviews_star = w_reviews['stars']
+	
+	feature_matrix = sk.feature_extraction.text.CountVectorizer(analyzer = textProcessing).fit_transform(reviews_text)
+	print feature_matrix.shape
 
 
-feature_matrix = sk.feature_extraction.text.CountVectorizer(analyzer = textProcessing).fit_transform(reviews_text)
-print feature_matrix.shape
 
-
-
-#print w_reviews.shape
